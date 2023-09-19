e9118ace615b9ebb19ef80bdceac7dd867bb1448
Joel Norrman <joel-norrman@hotmail.com>
2023-09-19 13:18:05 +0200

    scanner practice

--- src/com/Joel/scanner/Main.java
+++ src/com/Joel/scanner/Main.java
@@ -1,8 +1,37 @@
 package com.Joel.scanner;
 
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        boolean isPlaying = true;
 
+
+        do {
+
+          int result = scannerNum();
+            if (result > 1000){
+                break;
+            }
+          for (int i = 0; i < result; i++) {
+              System.out.println(i);
+              System.out.println("looping..");
+          }
+        } while (isPlaying);
+
+    }
+    public static int scannerNum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert number");
+        sc.hasNextInt();
+
+        return sc.nextInt();
+    }
+    public static String scannerText(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert sentence");
+        String input = sc.nextLine();
+        return input;
     }
 }
