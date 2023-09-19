67606becf37142db842849422bda29b2000869ce
Joel Norrman <joel-norrman@hotmail.com>
2023-09-19 13:41:42 +0200

    Student Scanner implementation

--- src/com/Joel/scanner/Main.java
+++ src/com/Joel/scanner/Main.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        //git push -u origin <branch name>
+        //Objects
         Scanner sc = new Scanner(System.in);
+        student bob = new student();
+
+        //Game
         do {
 
-            System.out.println("Input number");
+            System.out.println("Input bob's age");
             int result = sc.nextInt();
 
+            bob.age = result;
+
             System.out.println("You picked: " + result);
+            System.out.println("bob's age is: " + bob.age);
 
         } while (true);
 
---
+++ src/com/Joel/scanner/student.java
@@ -0,0 +1,10 @@
+package com.Joel.scanner;
+
+public class student {
+
+    public String name;
+    public int age;
+    public boolean isTired;
+
+
+}
